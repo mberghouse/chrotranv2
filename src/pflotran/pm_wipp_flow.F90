72a0c74bf346f0e224d5c204779e90359181fa15
Heeho Park <heepark@sandia.gov>
2020-04-22 14:34:07 -0600

    added pressure scaling cards

--- src/pflotran/pm_wipp_flow.F90
+++ src/pflotran/pm_wipp_flow.F90
@@ -66,7 +66,11 @@ module PM_WIPP_Flow_class
     PetscReal :: auto_pressure_Pb_0
     PetscReal :: auto_press_shallow_origin(3)
     PetscReal :: linear_system_scaling_factor
-    PetscBool :: scale_linear_system
+    PetscBool :: scale_linear_system ! Jacobian and residual is scaled 
+                                     ! just before the PETSc solver.
+    PetscBool :: scale_pressure ! pressure solution itself is scaled 
+                                ! this is used for advanced nonlinear methods
+    PetscReal :: pressure_scaling_factor ! user set parameter (default 1.d7)
     Vec :: scaling_vec
     ! When reading Dirichlet 2D Flared BC
     PetscInt, pointer :: dirichlet_dofs_ghosted(:) ! this array is zero-based indexing
@@ -198,6 +202,8 @@ subroutine PMWIPPFloInitObject(this)
   this%auto_press_shallow_origin = UNINITIALIZED_DOUBLE !this will default to dip rotation origin later
   this%linear_system_scaling_factor = 1.d7
   this%scale_linear_system = PETSC_TRUE
+  this%scale_pressure = PETSC_FALSE
+  this%pressure_scaling_factor = 1.d7
   this%scaling_vec = PETSC_NULL_VEC
   nullify(this%dirichlet_dofs_ghosted)
   nullify(this%dirichlet_dofs_ints)
