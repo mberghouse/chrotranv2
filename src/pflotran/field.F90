f78678c8159d9905170223661bfade4817f9e714
Heeho Park <heepark@sandia.gov>
2020-07-15 21:11:12 -0600

    v2.0 Scaled Jacobian properly by column scaling. seems to be working well.

--- src/pflotran/field.F90
+++ src/pflotran/field.F90
@@ -42,7 +42,7 @@ module Field_module
     Vec :: flow_xxdot, flow_xxdot_loc
 
     ! vectors for advanced nonlinear solvers other than Newton - Heeho
-    Vec :: flow_scaled_xx
+    Vec :: flow_scaled_xx, flow_work_loc
 
     ! vectors for operator splitting
     Vec :: tran_rhs
@@ -128,6 +128,7 @@ function FieldCreate()
   field%flow_xx = PETSC_NULL_VEC
   field%flow_xx_loc = PETSC_NULL_VEC
   field%flow_scaled_xx = PETSC_NULL_VEC
+  field%flow_work_loc = PETSC_NULL_VEC
   field%flow_dxx = PETSC_NULL_VEC
   field%flow_yy = PETSC_NULL_VEC
   field%flow_accum = PETSC_NULL_VEC
@@ -256,6 +257,9 @@ subroutine FieldDestroy(field)
   endif
   if (field%flow_scaled_xx /= PETSC_NULL_VEC) then
     call VecDestroy(field%flow_scaled_xx,ierr);CHKERRQ(ierr)
+  endif 
+  if (field%flow_work_loc /= PETSC_NULL_VEC) then
+    call VecDestroy(field%flow_work_loc,ierr);CHKERRQ(ierr)
   endif 
   if (field%flow_xx /= PETSC_NULL_VEC) then
     call VecDestroy(field%flow_xx,ierr);CHKERRQ(ierr)
