57808ca3b3beeee42b84ba363228b78607f4a61b
Heeho Park <heepark@sandia.gov>
2020-10-15 17:30:20 -0600

    remove too big rescue factor if statement

--- src/pflotran/timestepper_SNES.F90
+++ src/pflotran/timestepper_SNES.F90
@@ -299,6 +299,7 @@ subroutine TimestepperSNESUpdateDT(this,process_model)
     endif
     if (this%rescue_step_counter > this%rescue_frequency) then
       this%rescue_step_counter = 0
+#if 0
       if (2**this%max_time_step_cuts < this%rescue_factor) then
         ! can't jump timestep more than the max time step cut is allowed
         if (this%max_time_step_cuts < 2) then
@@ -312,6 +313,7 @@ subroutine TimestepperSNESUpdateDT(this,process_model)
       this%dt = this%dt * this%rescue_factor
       option%io_buffer = 'rescue mode activated. jumping time step size.'
       call PrintMsg(option)
+#endif
     endif
   endif
 
