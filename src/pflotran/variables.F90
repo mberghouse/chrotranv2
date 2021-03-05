eece3c4fae3b56eb874eccb89144101d1f65fcae
M. Rousseau <rousseau.moise@gmail.com>
2021-03-05 13:58:45 -0500

    remove liquid conductivity variables as it's not a face variable

--- src/pflotran/variables.F90
+++ src/pflotran/variables.F90
@@ -167,23 +167,14 @@ module Variables_module
   PetscInt, parameter, public :: TOTAL_BULK_CONC         = 138
   PetscInt, parameter, public :: MNRL_VOLUME_FRACTION    = 139
   
+  PetscInt, parameter, public :: ELECTRICAL_CONDUCTIVITY = 142
+  PetscInt, parameter, public :: ELECTRICAL_POTENTIAL    = 143
   
-  PetscInt, parameter, public :: LIQUID_CONDUCTIVITY     = 142
-  PetscInt, parameter, public :: LIQUID_CONDUCTIVITY_X   = 143
-  PetscInt, parameter, public :: LIQUID_CONDUCTIVITY_Y   = 144
-  PetscInt, parameter, public :: LIQUID_CONDUCTIVITY_Z   = 145
-  PetscInt, parameter, public :: LIQUID_CONDUCTIVITY_XY  = 146
-  PetscInt, parameter, public :: LIQUID_CONDUCTIVITY_YZ  = 147
-  PetscInt, parameter, public :: LIQUID_CONDUCTIVITY_XZ  = 148
-  
-  PetscInt, parameter, public :: FACE_PERMEABILITY       = 149
-  PetscInt, parameter, public :: FACE_AREA               = 150
-  PetscInt, parameter, public :: FACE_NON_ORTHO_ANGLE    = 151
-  PetscInt, parameter, public :: FACE_UPWIND_FRACTION    = 152
-  PetscInt, parameter, public :: FACE_DISTANCE_BETWEEN_CENTER = 153
+  PetscInt, parameter, public :: FACE_PERMEABILITY       = 144
+  PetscInt, parameter, public :: FACE_AREA               = 145
+  PetscInt, parameter, public :: FACE_NON_ORTHO_ANGLE    = 146
+  PetscInt, parameter, public :: FACE_UPWIND_FRACTION    = 147
+  PetscInt, parameter, public :: FACE_DISTANCE_BETWEEN_CENTER = 148
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 313d46560 (clean non implemented face output variable and add FACE_DISTANCE_BETWEEN_CENTER)
 end module Variables_module
