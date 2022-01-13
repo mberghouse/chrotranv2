92d0fdbdf6ebd98e035554f36a3262fb7b6f0461
Piyoosh Jaysaval <piyoosh.jaysaval01@gmail.com>
2022-01-13 16:52:38 -0600

    PM ZFLOW Transient Inversion: Adding changes for transient zflow inversion.

--- src/pflotran/zflow.F90
+++ src/pflotran/zflow.F90
@@ -110,6 +110,9 @@ subroutine ZFlowSetup(realization)
     enddo
   endif
 
+  ! ensure mapping of local cell ids to neighboring ghosted ids exits
+  call GridSetupCellNeighbors(grid,option)
+
   ! ensure that material properties specific to this module are properly
   ! initialized
   material_parameter => patch%aux%Material%material_parameter
