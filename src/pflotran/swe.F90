dccdeec3538c59d5b9bb04632fc29aae8bf5db43
Gautam Bisht <gautam.bisht@pnnl.gov>
2023-05-02 10:26:00 -0700

    Adds placeholder subroutines for SWE auxvars

--- src/pflotran/swe.F90
+++ src/pflotran/swe.F90
@@ -7,7 +7,8 @@ module SWE_module
 
   private
 
-  public :: SWESetup
+  public :: SWESetup, &
+            SWEUpdateAuxVars
 
 contains
 
@@ -22,17 +23,38 @@ subroutine SWESetup(surface_realization)
   !
   use Realization_Surface_class
   use Output_Aux_module
+  use Patch_module
+  use Option_module
+  use Grid_module
+  use SWE_Aux_module
 
   implicit none
 
   class (realization_surface_type) :: surface_realization
   type(output_variable_list_type), pointer :: list
 
+  type(option_type), pointer :: option
+  type(patch_type), pointer :: patch
+  type(grid_type), pointer :: grid
+  type(swe_auxvar_type), pointer :: swe_auxvars(:)
+  PetscInt :: ghosted_id
+
   list => surface_realization%output_option%output_snap_variable_list
   call SWESetPlotVariables(list)
 
   list => surface_realization%output_option%output_obs_variable_list
   call SWESetPlotVariables(list)
+
+  option => surface_realization%option
+  patch => surface_realization%patch
+  patch%surf_aux%SWE => SWEAuxCreate(option)
+
+  allocate(swe_auxvars(grid%ngmax))
+  do ghosted_id = 1, grid%ngmax
+    call SWEAuxVarInit(swe_auxvars(ghosted_id))
+  enddo
+  patch%surf_aux%SWE%auxvars => swe_auxvars
+  patch%surf_aux%SWE%num_aux = grid%ngmax
 
 end subroutine SWESetup
 
@@ -75,5 +97,82 @@ subroutine SWESetPlotVariables(list)
                                SURFACE_FLOW_Y_MOMENTUM)
 
 end subroutine SWESetPlotVariables
+
+! ************************************************************************** !
+
+subroutine SWEUpdateAuxVars(surface_realization)
+  !
+  ! Sets up the variable list for output and observation.
+  !
+  ! Author: Gautam Bisht
+  ! Date: 04/01/23
+  !
+  use Realization_Surface_class
+  use Patch_module
+  use Option_module
+  use Field_Surface_module
+  use Grid_module
+  use Coupler_module
+  use Connection_module
+  use Surface_Global_Aux_module
+  use SWE_Aux_module
+
+  implicit none
+
+  class (realization_surface_type) :: surface_realization
+
+  type(option_type), pointer :: option
+  type(patch_type), pointer :: patch
+  type(grid_type), pointer :: grid
+  type(field_surface_type), pointer :: field_surface
+  type(coupler_type), pointer :: boundary_condition
+  type(coupler_type), pointer :: source_sink
+  type(connection_set_type), pointer :: cur_connection_set
+  type(swe_auxvar_type), pointer :: swe_auxvars(:)
+  type(swe_auxvar_type), pointer :: swe_auxvars_bc(:)
+  type(swe_auxvar_type), pointer :: swe_auxvars_ss(:)
+  type(surface_global_auxvar_type), pointer :: surf_global_auxvars(:)
+  type(surface_global_auxvar_type), pointer :: surf_global_auxvars_bc(:)
+  type(surface_global_auxvar_type), pointer :: surf_global_auxvars_ss(:)
+
+  PetscInt :: ghosted_id, istart, iend
+  PetscReal, pointer :: xx_loc_p(:)
+  PetscErrorCode :: ierr
+
+  option => surface_realization%option
+  patch => surface_realization%patch
+  grid => patch%grid
+  field_surface => surface_realization%field_surface
+
+  swe_auxvars => patch%surf_aux%SWE%auxvars
+  swe_auxvars_bc => patch%surf_aux%SWE%auxvars_bc
+  swe_auxvars_ss => patch%surf_aux%SWE%auxvars_ss
+
+  surf_global_auxvars => patch%surf_aux%SurfaceGlobal%auxvars
+  surf_global_auxvars_bc => patch%surf_aux%SurfaceGlobal%auxvars_bc
+  surf_global_auxvars_ss => patch%surf_aux%SurfaceGlobal%auxvars_ss
+
+  call VecGetArrayF90(field_surface%flow_xx_loc,xx_loc_p, ierr);CHKERRQ(ierr)
+
+  do ghosted_id = 1, grid%ngmax
+    if (grid%nG2L(ghosted_id) < 0) cycle ! bypass ghosted corner cells
+
+    !Ignore inactive cells with inactive materials
+    if (associated(patch%imat)) then
+      if (patch%imat(ghosted_id) <= 0) cycle
+    endif
+
+    iend = ghosted_id*option%nflowdof
+    istart = iend - option%nflowdof + 1
+
+    call SWEAuxVarCompute(xx_loc_p(istart:iend), swe_auxvars(ghosted_id), &
+                          surf_global_auxvars(ghosted_id), option)
+
+  enddo
+
+
+  call VecRestoreArrayF90(field_surface%flow_xx_loc,xx_loc_p, ierr);CHKERRQ(ierr)
+
+end subroutine SWEUpdateAuxVars
 
 end module SWE_module
