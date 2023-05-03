44dbbccd57fbc71115a03255dbf2ab4d46a511db
Gautam Bisht <gautam.bisht@pnnl.gov>
2023-05-03 09:34:59 -0700

    Removes duplicate code and sets up PM linkage

--- src/pflotran/factory_surface_linkage.F90
+++ src/pflotran/factory_surface_linkage.F90
@@ -11,11 +11,63 @@ module Factory_Surface_linage_module
 
   private
 
-  public :: &
+  public :: FactorySurfaceLinkSetupPMApproach, &
             FactorySurfaceLinkExtractPMsFromPMList, &
             FactorySurfaceLinkSetupPMCLinages
 
 contains
+
+! ************************************************************************** !
+
+recursive subroutine FactorySurfaceLinkSetupPMApproach(pmc,simulation)
+!
+! Loops through all of the PMC's recursively and sets their realization,
+! timestepper, and solver.
+!
+! Author: Gautam Bisht
+! Date: 05/02/2023
+!
+#include "petsc/finclude/petscsnes.h"
+  use petscsnes
+  use Simulation_Surface_class
+  use PMC_Base_class
+  use PM_Base_class
+  use Option_module
+  use Realization_Surface_class
+  use PM_SWE_class
+
+  implicit none
+
+  class(simulation_surface_type) :: simulation
+  class(pmc_base_type), pointer :: pmc
+
+  class(realization_surface_type), pointer :: surface_realization
+  class(pm_base_type), pointer :: cur_pm
+  type(option_type), pointer :: option
+
+  surface_realization => simulation%surface_realization
+  option => surface_realization%option
+
+  if (.not.associated(pmc)) return
+
+  pmc%waypoint_list => simulation%waypoint_list_surface
+
+  ! loop through this pmc's process models:
+  cur_pm => pmc%pm_list
+  do
+    if (.not.associated(cur_pm)) exit
+
+    select type(cur_pm)
+      class is (pm_swe_type)
+        call cur_pm%SetRealization(surface_realization)
+    end select
+
+    cur_pm%output_option => simulation%output_option
+    call cur_pm%Setup()
+    cur_pm => cur_pm%next
+  enddo
+
+end subroutine FactorySurfaceLinkSetupPMApproach
 
 ! ************************************************************************** !
 
@@ -89,40 +141,14 @@ subroutine FactorySurfaceLinkSetupPMCLinages(simulation,pm_surface_flow)
   class(simulation_surface_type) :: simulation
   class(pm_surface_flow_type), pointer :: pm_surface_flow
 
-  class(pmc_surface_type), pointer :: pmc_surface
   type(option_type), pointer :: option
   class(realization_surface_type), pointer :: surface_realization
   type(input_type), pointer :: input
-
-  pmc_surface => PMCSurfaceCreate()
 
   call FactorySurfaceLinkAddPMCSurfaceFlow(simulation,pm_surface_flow,'PMCSurfaceFlow')
 
   surface_realization => simulation%surface_realization
   option => surface_realization%option
-
-  call pmc_surface%SetName('PMCSurfaceFlow')
-  call pmc_surface%SetOption(option)
-  call pmc_surface%SetWaypointList(simulation%waypoint_list_surface)
-
-  pmc_surface%pm_list => pm_surface_flow
-  pmc_surface%pm_ptr%pm => pm_surface_flow
-  pmc_surface%surface_realization => surface_realization
-
-  select type(pm_surface_flow)
-    class is (pm_swe_type)
-      pmc_surface%timestepper => TimestepperTSCreate()
-    class default
-      option%io_buffer = 'Unsupported PM in FactorySurfaceLinkSetupPMCLinages'
-      call PrintErrMsg(option)
-  end select
-  pmc_surface%timestepper%name = 'SURFACE_FLOW'
-
-  call pmc_surface%pm_list%InitializeSolver()
-
-  simulation%surface_flow_process_model_coupler => pmc_surface
-  simulation%surface_flow_process_model_coupler_list => &
-    simulation%surface_flow_process_model_coupler
 
   input => InputCreate(IN_UNIT,option%input_filename,option)
   call FactorySurfaceReadRequiredCards(simulation,input)
@@ -179,6 +205,10 @@ subroutine FactorySurfaceLinkAddPMCSurfaceFlow(simulation,pm_surface_flow,pmc_na
 
   call pmc_surface%pm_list%InitializeSolver()
   
-end subroutine FactorySurfaceLinkAddPMCSurfaceFlow
+  simulation%surface_flow_process_model_coupler => pmc_surface
+  simulation%surface_flow_process_model_coupler_list => &
+    simulation%surface_flow_process_model_coupler
+
+  end subroutine FactorySurfaceLinkAddPMCSurfaceFlow
 
 end module Factory_Surface_linage_module
