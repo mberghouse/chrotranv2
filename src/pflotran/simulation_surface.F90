1cf5f0a1d84d896b4aa120fcb983297b2fd50c25
Gautam Bisht <gautam.bisht@pnnl.gov>
2023-05-04 08:51:19 -0700

    Adds RunToTim for surface flow

--- src/pflotran/simulation_surface.F90
+++ src/pflotran/simulation_surface.F90
@@ -33,6 +33,8 @@ use Realization_Surface_class
     type(waypoint_list_type), pointer :: waypoint_list_surface
     type(waypoint_list_type), pointer :: waypoint_list_outer ! outer sync loop
   contains
+    procedure, public :: ExecuteRun => SimSurfaceExecuteRun
+    procedure, public :: RunToTime => SimSurfaceRunToTime
   end type simulation_surface_type
 
   public :: SimSurfaceCreate
@@ -94,7 +96,7 @@ subroutine SimSurfaceInit(this,driver,option)
   call SimulationBaseInit(this,driver)
   this%option => option
   this%output_option => OutputOptionCreate()
-  nullify(this%process_model_coupler_list)
+  nullify(this%surface_flow_process_model_coupler_list)
   nullify(this%process_model_list)
   this%sim_aux => SimAuxCreate()
   this%stop_flag = TS_CONTINUE
@@ -105,5 +107,122 @@ subroutine SimSurfaceInit(this,driver,option)
   this%waypoint_list_outer => WaypointListCreate()
 
 end subroutine SimSurfaceInit
+
+! ************************************************************************** !
+
+subroutine SimSurfaceExecuteRun(this)
+  !
+  ! Execute a simulation
+  !
+  ! Author: Gautam Bisht
+  ! Date: 05/02/23
+  !
+  use Waypoint_module
+  use Timestepper_Base_class, only : TS_CONTINUE
+  use Checkpoint_module
+
+  implicit none
+
+  class(simulation_surface_type) :: this
+
+  PetscReal :: final_time
+  type(waypoint_type), pointer :: cur_waypoint
+  character(len=MAXSTRINGLENGTH) :: append_name
+
+#ifdef DEBUG
+  call PrintMsg(this%option,'SimSurfaceExecuteRun()')
+#endif
+
+  if (.not.associated(this%surface_flow_process_model_coupler_list)) then
+    return
+  endif
+
+  final_time = SimSurfaceGetFinalWaypointTime(this)
+  cur_waypoint => this%waypoint_list_outer%first
+  if (cur_waypoint%print_checkpoint) then
+    append_name = &
+         CheckpointAppendNameAtTime(this%surface_flow_process_model_coupler_list% &
+                                        option%time, &
+                                    this%surface_flow_process_model_coupler_list%option)
+    call this%surface_flow_process_model_coupler_list%Checkpoint(append_name)
+  endif
+  call WaypointSkipToTime(cur_waypoint,this%option%time)
+  do
+    if (this%stop_flag /= TS_CONTINUE) exit ! end simulation
+    if (.not.associated(cur_waypoint)) exit
+    write(*,*)'final_time,cur_waypoint%time',final_time,cur_waypoint%time
+    call this%RunToTime(min(final_time,cur_waypoint%time))
+    cur_waypoint => cur_waypoint%next
+  enddo
+  append_name = '-restart'
+  if (associated(this%option%checkpoint)) then
+    call this%surface_flow_process_model_coupler_list%Checkpoint(append_name)
+  endif
+
+end subroutine SimSurfaceExecuteRun
+
+! ************************************************************************** !
+
+function SimSurfaceGetFinalWaypointTime(this)
+  !
+  ! Returns the earliest final waypoint time from the top layer of process
+  ! model couplers.
+  !
+  ! Author: Gautam Bisht
+  ! Date: 05/03/23
+  !
+  use Waypoint_module
+
+  implicit none
+
+  class(simulation_surface_type) :: this
+
+  PetscReal :: SimSurfaceGetFinalWaypointTime
+
+  class(pmc_base_type), pointer :: cur_process_model_coupler
+  PetscReal :: final_time
+
+  SimSurfaceGetFinalWaypointTime = 0.d0
+
+  cur_process_model_coupler => this%surface_flow_process_model_coupler_list
+  do
+    if (.not.associated(cur_process_model_coupler)) exit
+    final_time = WaypointListGetFinalTime(cur_process_model_coupler% &
+                                            waypoint_list)
+    if (SimSurfaceGetFinalWaypointTime < 1.d-40 .or. &
+        final_time < SimSurfaceGetFinalWaypointTime) then
+          SimSurfaceGetFinalWaypointTime = final_time
+    endif
+    cur_process_model_coupler => cur_process_model_coupler%peer
+  enddo
+
+end function SimSurfaceGetFinalWaypointTime
+
+! ************************************************************************** !
+
+subroutine SimSurfaceRunToTime(this,target_time)
+  !
+  ! Executes simulation
+  !
+  ! Author: Gautam Bisht
+  ! Date: 05/03/23
+  !
+  use Option_module
+  use Simulation_Aux_module
+
+  implicit none
+
+  class(simulation_surface_type) :: this
+  PetscReal :: target_time
+
+#ifdef DEBUG
+  call PrintMsg(this%option,'SimSurfaceRunToTime()')
+#endif
+
+  write(*,*)'In SimSurfaceRunToTime'
+  write(*,*)'  call this%surface_flow_process_model_coupler_list%RunToTime(): target_time = ',target_time
+  call this%surface_flow_process_model_coupler_list%RunToTime(target_time,this%stop_flag)
+
+end subroutine SimSurfaceRunToTime
 
 end module Simulation_Surface_class
