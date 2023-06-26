c1a658a2877e74ed4aeaff100807c6391dbe4c89
Calvin Madsen <madsenc004@gmail.com>
2023-06-26 12:00:20 -0600

    bash instead of sh

--- .gitlab/build-petsc-intel.sh
+++ .gitlab/build-petsc-intel.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # configure intel
-. ./opt/intel/oneapi/setvars.sh
+source /opt/intel/oneapi/setvars.sh
 export PATH=/opt/intel/oneapi/mpi/2021.9.0/bin:/opt/intel/oneapi/compiler/2023.1.0/linux/bin/intel64:/opt/intel/oneapi/compiler/2023.1.0/linux/bin:$PATH
 ifort --version
 icc --version
