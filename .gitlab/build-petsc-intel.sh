bada0bbfc5ff2de94d7078c8c7d66e32556c2e7b
Calvin Madsen <madsenc004@gmail.com>
2023-06-26 12:23:34 -0600

    changed compiler paths from mpich dir to intel installed path

--- .gitlab/build-petsc-intel.sh
+++ .gitlab/build-petsc-intel.sh
@@ -14,7 +14,7 @@ MPICH_INSTALL_DIR=$MPICH_DIR/install
 cd $MPICH_DIR
 
 # from petsc 3.18 --download-mpich=yes configure  for mpich 4.0.2
-./configure --prefix=$MPICH_INSTALL_DIR MAKE=/usr/bin/gmake --libdir=$MPICH_INSTALL_DIR/lib CC=icc CFLAGS="-g -O0 -fPIC" AR=/usr/bin/ar ARFLAGS=cr CXX=icpc CXXFLAGS="-g -O0" FFLAGS="-g -O0 -Wno-unused-function -fallow-argument-mismatch" FC=ifort F77=ifort FCFLAGS="-g -O0 -Wno-unused-function -fallow-argument-mismatch" --disable-shared --with-pm=hydra --disable-java --with-hwloc=embedded --enable-fast=no --enable-error-messages=all --with-device=ch3:sock --enable-g=meminit
+./configure --prefix=$MPICH_INSTALL_DIR MAKE=/usr/bin/gmake --libdir=$MPICH_INSTALL_DIR/lib CC=icc CFLAGS="-g -O0 -fPIC" AR=/usr/bin/ar ARFLAGS=cr CXX=icpc CXXFLAGS="-g -O0" FFLAGS="-g -O0 -Wno-unused-function" FC=ifort F77=ifort FCFLAGS="-g -O0 -Wno-unused-function" --disable-shared --with-pm=hydra --disable-java --with-hwloc=embedded --enable-fast=no --enable-error-messages=all --with-device=ch3:sock --enable-g=meminit
 make all; make install
 
 # then clone and build petsc
@@ -22,9 +22,9 @@ git clone https://gitlab.com/petsc/petsc.git $PETSC_DIR
 cd $PETSC_DIR
 git checkout $PETSC_VERSION
 ./configure PETSC_ARCH=petsc-arch \
---with-cc=$MPICH_INSTALL_DIR/bin/mpiicc \
---with-cxx=$MPICH_INSTALL_DIR/bin/mpiicpc \
---with-fc=$MPICH_INSTALL_DIR/bin/mpiifort \
+--with-cc=mpiicc \
+--with-cxx=mpiicpc \
+--with-fc=mpiifort \
 --COPTFLAGS='-g -O0' --CXXOPTFLAGS='-g -O0' --FOPTFLAGS='-g -O0 -Wno-unused-function' --with-clanguage=c --with-debug=1 --with-shared-libraries=0 --download-hdf5 --download-metis --download-parmetis --download-fblaslapack --download-hypre --download-hdf5-fortran-bindings=yes
 make
 rm -Rf petsc-arch/externalpackages
